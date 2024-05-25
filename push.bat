b321ed25dd272646d9e0adbb3d7863d7d8113cfa
daisukeland <daisukeland4@gmail.com>
2024-05-25 18:33:04 -0400

    first commit

---
+++ push.bat
@@ -0,0 +1,9 @@
+echo "# scripts" >> README.md
+git init
+git add .
+git config --global user.email "daisukeland4@gmail.com"
+git config --global user.name "daisukeland"
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/daisukeland/scripts.git
+git push -u origin main
